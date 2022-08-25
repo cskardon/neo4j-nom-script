5c405810aac77189e30f3037abe126fde7a1ff34
Charlotte Skardon <charlotte@tournr.com>
2022-08-25 11:39:54 +0100

    Upgrading to NOM server `1.1.1`

--- Windows/Scripts/version.ps1
+++ Windows/Scripts/version.ps1
@@ -5,7 +5,7 @@ $zuluVersion = "11.58.15"
 $jreVersion = "11.0.16"
 $nomZuluVersion = "17.36.13"
 $nomJreVersion = "17.0.4"
-$nomVersion = "1.1.0"
+$nomVersion = "1.1.1"
 $openSslVersion = "1.1.1h"
 
 Write-Host " Done!" -ForegroundColor Green
