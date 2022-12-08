bd8744bb0d5596f777d5c75f39b66d71e547449f
Charlotte Skardon <charlotte@tournr.com>
2022-12-08 14:49:39 +0000

    Neo4j to `5.2.0`

--- Windows/Scripts/version.ps1
+++ Windows/Scripts/version.ps1
@@ -1,6 +1,6 @@
 Write-Host "Setting the versions ..." -NoNewline;
 
-$neo4jVersion = "5.1.0"
+$neo4jVersion = "5.2.0"
 $javaVersion = "17"
 $temurinURLVersion="17.0.5%2B8"
 $temurinFileVersion="17.0.5_8"
